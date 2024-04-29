25b788551036df16f6c4a33274c663b457762fba
Johannes Bekele <76441090+JohannesSBA@users.noreply.github.com>
2024-04-29 15:37:02 -0400

    feat: barbarian

---
+++ barbarian.gd
@@ -0,0 +1,13 @@
+extends Node3D
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	$AnimationPlayer.play("Idle")
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
