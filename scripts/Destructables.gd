16c02a2da6b158f4ec0eea0d569236616bac7fc1
Johannes Bekele <76441090+JohannesSBA@users.noreply.github.com>
2024-05-02 10:59:54 -0400

    Pulled

---
+++ scripts/Destructables.gd
@@ -0,0 +1,19 @@
+extends GridMap
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
+
+func destroy_cell(collision_point):
+	var scale_point = collision_point
+	var NodeLocal = to_local(scale_point)
+	var map_coordinate = local_to_map(NodeLocal)
+	set_cell_item(map_coordinate, -1)
+
+	
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ scripts/spell_trail.gd
@@ -0,0 +1,30 @@
+extends Node3D
+
+var SPEED = 10.0
+
+@onready var mesh = $MeshInstance3D
+@onready var ray = $RayCast3D
+@onready var particle = $GPUParticles3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(delta):
+	position += transform.basis * Vector3(0,0,-SPEED) * delta
+	ray.force_raycast_update()
+	if ray.is_colliding():
+		if ray.get_collider().has_method("destroy_cell"):
+			SPEED=0
+			mesh.visible = false
+			particle.emitting = true
+			#print(ray.get_collision_point() - ray.get_collision_normal())
+			ray.get_collider().destroy_cell(ray.get_collision_point() - ray.get_collision_normal())
+		await get_tree().create_timer(1.0).timeout
+		queue_free()
+
+
+
+func _on_timer_timeout():
+	queue_free()
