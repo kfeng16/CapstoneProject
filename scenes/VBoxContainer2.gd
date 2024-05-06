724c7437c341623aa489c8919c40b61603f23de0
Kristine Feng <kfeng16@fordham.edu>
2024-05-06 13:41:31 -0400

    c
    c

---
+++ scenes/VBoxContainer2.gd
@@ -0,0 +1,12 @@
+extends VBoxContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+var finishline = new()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
