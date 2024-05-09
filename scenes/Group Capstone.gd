4fe382961f3b5aefff5b306fcf67baa82fc52cac
Jay <jmcook5520@gmail.com>
2024-05-09 13:29:57 -0400

    Successful Win Screen

--- scenes/Group Capstone.gd
+++ scenes/Group Capstone.gd
@@ -4,3 +4,11 @@ extends Node3D
 
 func _physics_process(delta):
 	get_tree().call_group("enemies", "update_target_location", player.global_transform.origin)
+
+
+func _on_area_3d_area_shape_entered(area_rid, area, area_shape_index, local_shape_index):
+	get_tree().change_scene_to_file("res://scenes/Win Screen.tscn")
+
+
+func _on_area_3d_body_entered(body):
+	get_tree().change_scene_to_file('res://scenes/Win Screen.tscn')
